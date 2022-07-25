052705f0d1cb39bf9bacc40f26491a90a2c7fdef
NoobMaster <bk201_perez@yahoo.com>
2022-07-25 10:01:39 +0800

    uploading whole finished act

---
+++ app/src/main/java/com/example/sqliteoperation/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.sqliteoperation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import java.text.BreakIterator;
+
+public class MainActivity extends AppCompatActivity {
+    EditText Name, Pass, Delete, UpdateS1, UpdateS2;
+    myDbAdapter helper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Name = (EditText) findViewById(R.id.editName);
+        Pass = (EditText) findViewById(R.id.editPass);
+        Delete = (EditText) findViewById(R.id.editText6);
+        UpdateS1 = (EditText) findViewById(R.id.editText3);
+        UpdateS2 = (EditText) findViewById(R.id.editText5);
+
+        helper = new myDbAdapter(this);
+    }
+    public void addUser(View view)
+    {
+        String t1 = Name.getText().toString();
+        String t2 = Pass.getText().toString();
+        if(t1.isEmpty() || t2.isEmpty())
+        {
+            Message.message(getApplicationContext(),"Enter Both Name and Password");
+        }
+        else
+        {
+            long id = helper.insertData(t1,t2);
+            if(id<=0)
+            {
+                Message.message(getApplicationContext(),"Insertion Unsuccessful");
+            } else
+            {
+                Message.message(getApplicationContext(),"Insertion Successful");
+            }
+            Name.setText("");
+            Pass.setText("");
+        }
+    }
+    public void viewdata(View view)
+    {
+        String data = helper.getData();
+        Message.message(this,data);
+    }
+
+
+
+    public void delete( View view)
+    {
+        String uname = Delete.getText().toString();
+        if(uname.isEmpty())
+        {
+            Message.message(getApplicationContext(),"Enter Data");
+        }
+        else{
+            int a= helper.delete(uname);
+            if(a<=0)
+            {
+                Message.message(getApplicationContext(),"Unsuccessful");
+                Delete.setText("");
+            }
+            else
+            {
+                Message.message(this, "DELETED");
+                Delete.setText("");
+            }
+        }
+    }
+
+    public void update( View view)
+    {
+        String u1 = UpdateS1.getText().toString();
+        String u2 = UpdateS2.getText().toString();
+        if(u1.isEmpty() || u2.isEmpty())
+        {
+            Message.message(getApplicationContext(),"Enter Data");
+        }
+        else
+        {
+            int a= helper.updateName( u1, u2);
+            if(a<=0)
+            {
+                Message.message(getApplicationContext(),"Unsuccessful");
+                UpdateS1.setText("");
+                UpdateS2.setText("");
+            } else {
+                Message.message(getApplicationContext(),"Updated");
+                UpdateS1.setText("");
+                UpdateS2.setText("");
+            }
+        }
+    }
+}
+
